10d1eeb0d7612939ef09f7860a4ee82718bae414
Aysuarex <aysuarex@gmail.com>
2022-06-20 22:28:18 +0100

    Fixing file to compile with pycodestyle

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,8 +1,20 @@
 #!/usr/bin/python3
+# 1-safe_print_integer.py
+# Brennan D Baraban <375@holbertonschool.com>
+
+
 def safe_print_integer(value):
+    """Print an integer with "{:d}".format().
+
+    Args:
+        value (int): The integer to print.
+
+    Returns:
+        If a TypeError or ValueError occurs - False.
+        Otherwise - True.
+    """
     try:
         print("{:d}".format(value))
-    except:
-        return (False)
-    else:
         return (True)
+    except (TypeError, ValueError):
+        return (False)
