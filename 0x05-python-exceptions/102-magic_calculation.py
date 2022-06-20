dfe818c004b5dcd2b21cf44af3a931789ba8a6c1
Ayomide Suara <99369085+Aysuarex@users.noreply.github.com>
2022-06-20 14:42:18 -0700

    Fixed Whitespace Error

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -1,17 +1,12 @@
 #!/usr/bin/python3
-# 102-magic_calculation.py
-# Brennan D Baraban <375@holbertonschool.com>
-
-
 def magic_calculation(a, b):
     result = 0
     for i in range(1, 3):
         try:
             if i > a:
                 raise Exception('Too far')
-            else:
-                result += a ** b / i
-        except:
+            result += a ** b / i
+        except Exception:
             result = b + a
             break
-    return (result)
+    return result
