5c0b092bcfb7b1e94120adc627c8cfedf923da1d
Aysuarex <aysuarex@gmail.com>
2022-06-20 22:29:05 +0100

    Adjusted file content to linter preferences

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,9 +1,14 @@
 #!/usr/bin/python3
+# 3-safe_print_division.py
+# Brennan D Baraban <375@holbertonschool.com>
+
+
 def safe_print_division(a, b):
+    """Returns the division of a by b."""
     try:
-        result = a / b
-    except:
-        result = None
+        div = a / b
+    except (TypeError, ZeroDivisionError):
+        div = None
     finally:
-        print("Inside result: {}".format(result))
-        return (result)
+        print("Inside result: {}".format(div))
+    return (div)
