76ed35e159368eb48d207f7d72de73552952ed1e
Aysuarex <aysuarex@gmail.com>
2022-06-20 22:29:37 +0100

    Pycodestyle linting

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
+# 102-magic_calculation.py
+# Brennan D Baraban <375@holbertonschool.com>
+
+
 def magic_calculation(a, b):
     result = 0
     for i in range(1, 3):
         try:
-            if (i > a):
-                raise Exception("Too far")
+            if i > a:
+                raise Exception('Too far')
             else:
-                result += (a ** b) / i
+                result += a ** b / i
         except:
             result = b + a
             break
