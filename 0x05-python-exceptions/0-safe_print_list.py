4fede481cf3e5721fb4052f7460a47a5a363858b
Aysuarex <aysuarex@gmail.com>
2022-06-20 22:27:38 +0100

    Debugged with PEP8 directives

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,14 +1,24 @@
 #!/usr/bin/python3
-def safe_print_list(my_list=[], x=0):
-    count = 0
+# 0-safe_print_list.py
+# Brennan D Baraban <375@holbertonschool.com>
 
+
+def safe_print_list(my_list=[], x=0):
+    """Print x elememts of a list.
+
+    Args:
+        my_list (list): The list to print elements from.
+        x (int): The number of elements of my_list to print.
+
+    Returns:
+        The number of elements printed.
+    """
+    ret = 0
     for i in range(x):
         try:
-            print("{}".format(my_list[i]), end='')
-        except:
+            print("{}".format(my_list[i]), end="")
+            ret += 1
+        except IndexError:
             break
-        else:
-            count += 1
-
-    print()
-    return (count)
+    print("")
+    return (ret)
