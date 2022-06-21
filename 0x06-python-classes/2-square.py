2314658f965c5c99a2bca8c5c98f82e49f986655
Aysuarex <aysuarex@gmail.com>
2022-06-21 20:50:31 +0100

    Mandatory Task 2

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+class Square:
+    """ A class that defines a square by its size
+    """
+    def __init__(self, size=0):
+        """ Method to initialize the square object
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = int(size)
