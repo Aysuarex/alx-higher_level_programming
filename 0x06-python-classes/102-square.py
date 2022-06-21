929fc3bfd350289dbfd26cdbb9ef1d9890945962
Aysuarex <aysuarex@gmail.com>
2022-06-21 20:52:34 +0100

    Advanced Task 2

---
+++ 0x06-python-classes/102-square.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+class Square:
+    """ A class that defines a square by its size
+    """
+    def __eq__(self, other):
+        return self.__size == other.__size
+
+    def __lt__(self, other):
+        return self.__size < other.__size
+
+    def __le__(self, other):
+        return self.__size <= other.__size
+
+    def __ne__(self, other):
+        return self.__size != other.__size
+
+    def __gt__(self, other):
+        return self.__size > other.__size
+
+    def __ge__(self, other):
+        return self.__size >= other.__size
+
+    def __init__(self, size=0):
+        """ Method to initialize the square object
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """ Method that returns the square are of the object
+        """
+        return (self.__size ** 2)
+
+    @property
+    def size(self):
+        """ Method to returns the size value
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """ Method to set the size value of the square object
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
