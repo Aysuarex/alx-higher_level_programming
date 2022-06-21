272bbb8b95106463815d8fb67206904a99816b9b
Aysuarex <aysuarex@gmail.com>
2022-06-21 20:50:09 +0100

    Mandatory Task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+class Square:
+    """Class Square that defines a square object
+    """
+    def __init__(self, size):
+        """Initialize method that stores the size of the square
+
+        Args:
+            param1 (int): size of the square
+        """
+        self.__size = size
