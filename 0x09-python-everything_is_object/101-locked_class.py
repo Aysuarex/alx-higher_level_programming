6706b319902f69f12995cf1b1eb972d8987f329e
Aysuarex <aysuarex@gmail.com>
2022-06-29 00:34:02 +0100

    Changed single quotes to double

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
-""" LockedClass
-"""
+"""Defines a locked class."""
 
 
 class LockedClass:
-    __slots__ = ['first_name']
+    """
+    Prevent the user from instantiating new LockedClass attributes
+    for anything but attributes called 'first_name'.
+    """
+
+    __slots__ = ["first_name"]
