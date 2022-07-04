cba7a694c8b951958bfafb22fc5b3ca0ca7ccd89
Aysuarex <aysuarex@gmail.com>
2022-07-04 13:22:29 +0100

    adding docstring to file

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,15 +1,17 @@
-#!/usr/bin/python3
-""" module square"""
+#/usr/bin/python3
+"""Defines a Rectangle subclass Square."""
 Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
-    def __init__(self, size):
-        """ init rectangle """
-        self.integer_validator("size", size)
-        Rectangle.__init__(self, size, size)
-        self.__size = size
+    """Represent a square."""
 
-    def __str__(self):
-        """ str version  """
-        return "[Square] {}/{}".format(self.__size, self.__size)
+    def __init__(self, size):
+        """Initialize a new square.
+
+        Args:
+            size (int): The size of the new square.
+        """
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
