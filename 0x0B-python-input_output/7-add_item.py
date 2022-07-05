09a2c690901052cb5643d93912639c8ed3c8bc40
Aysuarex <aysuarex@gmail.com>
2022-07-05 13:37:37 +0100

    Task 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -7,17 +7,3 @@ save them to a file
 import sys
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
-
-
-try:
-    new_list = load_from_json_file('add_item.json')
-except:
-    new_list = []
-
-for i in range(1, len(sys.argv)):
-    new_list.append(sys.argv[i])
-
-try:
-    save_to_json_file(new_list, 'add_item.json')
-except:
-    pass
