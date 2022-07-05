ca62c8b5fc4552ee9cf516f1358009b05b6e654c
Aysuarex <aysuarex@gmail.com>
2022-07-05 13:33:39 +0100

    Corrected appropriate naming

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -5,8 +5,8 @@ save them to a file
 """
 
 import sys
-save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 
 try:
