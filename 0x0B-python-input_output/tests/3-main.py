198455f248ccdf58ad5dd07d29f163f699124ac5
Aysuarex <aysuarex@gmail.com>
2022-07-05 21:48:03 +0100

    main.py test file for task 3

--- 0x0B-python-input_output/tests/3-main.py
+++ 0x0B-python-input_output/tests/3-main.py
@@ -1,5 +1,29 @@
 #!/usr/bin/python3
-write_file = __import__('3-write_file').write_file
+to_json_string = __import__('5-to_json_string').to_json_string
 
-nb_characters = write_file("my_first_file.txt", "Holberton School is so cool!\n")
-print(nb_characters)
+my_list = [1, 2, 3]
+s_my_list = to_json_string(my_list)
+print(s_my_list)
+print(type(s_my_list))
+
+my_dict = { 
+    'id': 12,
+    'name': "John",
+    'places': [ "San Francisco", "Tokyo" ],
+    'is_active': True,
+    'info': {
+        'age': 36,
+        'average': 3.14
+    }
+}
+s_my_dict = to_json_string(my_dict)
+print(s_my_dict)
+print(type(s_my_dict))
+
+try:
+    my_set = { 132, 3 }
+    s_my_set = to_json_string(my_set)
+    print(s_my_set)
+    print(type(s_my_set))
+except Exception as e:
+    print("[{}] {}".format(e.__class__.__name__, e))
