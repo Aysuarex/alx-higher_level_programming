3821f3ec3ca6a3b44c1b2c30c4cadde2d64bf8e4
Aysuarex <aysuarex@gmail.com>
2022-08-12 12:04:15 +0100

    Fixed SQL Styling

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -4,6 +4,6 @@ SELECT `id`, `name`
   FROM `cities`
  WHERE `state_id` IN
        (SELECT `id`
-	  FROM `states`
-	 WHERE `name` = "California")
- ORDER BY `id`;
+	        FROM `states`
+	       WHERE `name` = "California")
+ORDER BY `id`;
