0b8b038e7bbedcbf8c45a3e46736b8cc30ff1465
Aysuarex <aysuarex@gmail.com>
2022-09-11 22:59:06 +0100

    .py file for advanced task 0"

---
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" prints the State object with the name passed as argument from the database
+"""
+import sys
+from relationship_state import Base, State
+from relationship_city import City
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import relationship
+
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    new_state = State(name='California')
+    new_city = City(name='San Francisco')
+    new_city.state = new_state
+    session.add(new_city, new_state)
+    session.commit()
