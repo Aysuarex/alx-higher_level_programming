73cddce835c203341645527ce212d2aa50b31d9a
Aysuarex <aysuarex@gmail.com>
2022-09-23 01:48:33 +0100

    File for mandatory task 0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-#takes in a URL, sends a request to that URL, and displays the size of the body of the response
-curl -sI "$1" | grep Content-Length: | cut -d " " -f 2
+# Get the byte size of the HTTP response header for a given URL.
+curl -s "$1" | wc -c
