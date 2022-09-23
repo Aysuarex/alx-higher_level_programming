a25269e28c6c503eb9fd8d09dde4a4ab3d3690b5
Ayomide Suara <aysuarex@gmail.com>
2022-09-23 07:14:32 -0700

    Update 4-hbtn_status.py

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-"""fetches https://intranet.hbtn.io/status."""
-import requests
+"""
+requests model
+"""
 
-
-if __name__ == "__main__":
-    r = requests.get("https://intranet.hbtn.io/status")
+if __name__ == '__main__':
+    import requests
+    html = requests.get('https://alx-intranet.hbtn.io/status')
     print("Body response:")
-    print("\t- type: {}".format(type(r.text)))
-    print("\t- content: {}".format(r.text))
+    print("\t- type: {}".format(html.text.__class__))
+    print("\t- content: {}".format(html.text))
