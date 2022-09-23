6b38decdfe229511e19e140d5f1fba6feb5c1a77
Aysuarex <aysuarex@gmail.com>
2022-09-23 01:56:54 +0100

    Updated file for advanced task 2

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# ends a JSON POST request to a URL passed as the first argument, and displays the body of the response.
-curl -sL -X PUT 0.0.0.0:5000/catch_me -d "user_id=98" -H "Origin: HolbertonSchool"
+# Makes a request to 0.0.0.0:5000/catch_me that gets the message "You got me!".
+curl -sL -X PUT -H "Origin: HolbertonSchool" -d "user_id=98" 0.0.0.0:5000/catch_me
