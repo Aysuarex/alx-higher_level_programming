a5a837517b032d6bf258f210364fbc7233f41047
Aysuarex <aysuarex@gmail.com>
2022-09-23 01:55:59 +0100

    Update task 4 file

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-#  takes in a URL as an argument, sends a GET request to the URL, and displays the body of the response
-curl -sX GET -H 'X-HolbertonSchool-User-Id: 98' "$1"
+# Send a GET request to a given URL with a header variable.
+curl -sH "X-HolbertonSchool-User-Id: 98" "$1"
