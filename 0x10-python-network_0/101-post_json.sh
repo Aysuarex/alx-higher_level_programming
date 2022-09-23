4ecc331c74628f7a246f8dc83cece162c5c4d7f8
Aysuarex <aysuarex@gmail.com>
2022-09-23 01:56:43 +0100

    Updated file for advanced task 1

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# ends a JSON POST request to a URL passed as the first argument, and displays the body of the response.
-curl -sX POST -H "Content-Type: application/json" -d @"$2" "$1"
+# Sends a JSON POST request to a given URL with a given JSON file.
+curl -s -H "Content-Type: application/json" -d "$(cat "$2")" "$1"
