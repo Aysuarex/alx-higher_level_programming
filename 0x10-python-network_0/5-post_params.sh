b96a043c69da5e5760f5618095e6536771de19fe
Aysuarex <aysuarex@gmail.com>
2022-09-23 01:56:14 +0100

    Update task 5 file

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# takes in a URL, sends a POST request to the passed URL, and displays the body of the response
- curl -sX POST "$1" -d "email=hr@holbertonschool.com&subject=I%20will%20always%20be%20here%20for%20PLD" 
+# Bash scripts that sends a POST request to a given URL.
+curl -s -X POST -d "email=hr@holbertonschool.com&subject=I will always be here for PLD" "$1"
