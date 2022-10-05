01947bae0518a08007aee4db5a77ba77e95e11b9
Aysuarex <aysuarex@gmail.com>
2022-10-05 20:53:40 +0100

    ALX-given test files

--- 0x0B-python-input_output/tests/1-main.py
+++ 0x0B-python-input_output/tests/1-main.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 write_file = __import__('3-write_file').write_file
 
-nb_characters = write_file("my_first_file.txt", "Holberton School is so cool!\n")
+nb_characters = write_file("my_first_file.txt", "School is so cool!\n")
 print(nb_characters)
