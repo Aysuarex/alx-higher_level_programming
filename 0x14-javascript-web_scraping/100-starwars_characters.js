2123ea9489051648272dbbd55965338b944adabf
Aysuarex <aysuarex@gmail.com>
2022-10-18 20:11:44 +0100

    Updated 100-starwars_chracters.js file

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,17 +1,15 @@
 #!/usr/bin/node
-const id = process.argv[2];
-const url = 'http://swapi.co/api/films/' + id;
 const request = require('request');
+const url = 'http://swapi.co/api/films/' + process.argv[2];
+
 request(url, function (err, response, body) {
-  if (err) {
-    throw err;
-  } else {
-    for (const charUrl of JSON.parse(body).characters) {
-      request(charUrl, function (err2, response2, body2) {
-        if (err2) {
-          throw err2;
-        } else {
-          console.log(JSON.parse(body2).name);
+  if (err == null) {
+    const resp = JSON.parse(body);
+    const characters = resp.characters;
+    for (let i = 0; i < characters.length; i++) {
+      request(characters[i], function (err, response, body) {
+        if (err == null) {
+          console.log(JSON.parse(body).name);
         }
       });
     }
