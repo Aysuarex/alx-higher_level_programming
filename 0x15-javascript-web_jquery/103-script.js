d4f69df80f149a949fef45f9bd77eddc7377d8a2
Aysuarex <aysuarex@gmail.com>
2022-10-27 00:28:01 +0100

    Advanced Task 3

---
+++ 0x15-javascript-web_jquery/103-script.js
@@ -0,0 +1,22 @@
+$(document).ready(function () {
+  function translate () {
+    $('DIV#hello').empty();
+    const len = $('INPUT#language_code').val();
+    $.ajax({
+      type: 'GET',
+      url: 'https://fourtonfish.com/hellosalut/?lang=' + len,
+      success: function (data) {
+        $('DIV#hello').append(data.hello);
+      }
+    });
+  }
+  $('INPUT#btn_translate').click(function () {
+    translate();
+  });
+  $('INPUT#language_code').keypress(function (e) {
+    const key = e.which;
+    if (key === 13) {
+      translate();
+    }
+  });
+});
